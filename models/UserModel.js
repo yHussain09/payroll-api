cc9c31f47b462f60a7404048e1dd7a83538c4717
yHussain09 <y.hussain09@gmail.com>
2023-05-12 18:16:47 +0500

    add basic validation.

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    firstName:String,
+    firstName: {
+        type: String, 
+        required: [true, 'must provide first name'],
+        trim: true,
+        maxLength: [20, 'first name not more than 20 characters']
+    },
     lastName:String,
     dateOfBirth:String,
     userName:String,
     password:String,
     contactNumber:String,
+    isAdmin: {
+        type:Boolean,
+        default: false
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
